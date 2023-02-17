a953f95671e5b25fed15f9dfb58317bb702f3a7f
rob chaplin <rob.chaplin@bybox.com>
2023-02-17 15:24:47 +0000

    empty app

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+declare let window: any;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+if (window.cordova) {
+    document.addEventListener('deviceready', () => {
+        root.render(
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        );
+
+    }, false);
+} else {
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
